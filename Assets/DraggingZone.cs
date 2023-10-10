5e376ea673d4bf305a2cf6aea3b1365320ce0eb3
Jari Bou <bourdietome@gmail.com>
2023-10-10 10:48:10 +0200

    Fixed draggable

--- Assets/DraggingZone.cs
+++ Assets/DraggingZone.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,10 +15,22 @@ namespace ProjectClicker
         [SerializeField] private Transform _camera;
 
         [SerializeField] private float _dragSpeed = 0.5f;
+
+        [SerializeField] private float _maxWidthDrag = 120;
+
+        private float _cameraSize;
+
+        private Vector3 _startCamPos;
+        private float xStartPos;
+        private float yStartPos;
+        private float zStartPos;
         // Start is called before the first frame update
         void Start()
         {
-            
+            xStartPos = _camera.transform.position.x;
+            yStartPos = _camera.transform.position.y;
+            zStartPos = _camera.transform.position.z;
+            _startCamPos = _camera.transform.position;
         }
 
         // Update is called once per frame
@@ -31,7 +44,10 @@ namespace ProjectClicker
             if (!_dragging) return;
             
             Vector2 displacement = eventData.delta*-1;
-            _camera.transform.Translate(displacement.x * _dragSpeed, 0, 0);
+            float xClamped = Mathf.Clamp(_camera.transform.position.x + displacement.x * _dragSpeed,
+                _startCamPos.x-_maxWidthDrag, _startCamPos.x+_maxWidthDrag);
+            Vector3 newPos = new Vector3(xClamped, _startCamPos.y, _startCamPos.z);
+            _camera.transform.position = newPos;
         }
 
         public void OnPointerDown(PointerEventData eventData)
@@ -43,5 +59,18 @@ namespace ProjectClicker
         {
             _dragging = false;
         }
+
+        #if  UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireCube(_startCamPos, new Vector3(2*(_cameraSize + _maxWidthDrag), _cameraSize*2, 0));
+        }
+
+        private void OnValidate()
+        {
+            _cameraSize = _camera.GetComponent<Camera>().orthographicSize;
+        }
+#endif
     }
 }
