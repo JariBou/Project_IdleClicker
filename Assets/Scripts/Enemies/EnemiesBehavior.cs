efec4be31aa3d887a04a289e30da64495d061af6
Shahine <shahine.benthanane@edu.devinci.fr>
2023-12-26 23:23:47 +0100

    Ajout de la Health Bar Enemie et Background

--- Assets/Scripts/Enemies/EnemiesBehavior.cs
+++ Assets/Scripts/Enemies/EnemiesBehavior.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.UI;
+
 
 namespace ProjectClicker
 {
@@ -9,6 +11,8 @@ namespace ProjectClicker
         public float health;
         public float maxHealth;
         bool isDead;
+        [SerializeField] private Slider _healthBar;
+
 
         [Header("Attack")]
         [SerializeField] float AttackRange;
@@ -16,7 +20,8 @@ namespace ProjectClicker
         // Start is called before the first frame update
         void Start()
         {
-        
+            _healthBar.maxValue = maxHealth;
+            _healthBar.value = health;
         }
 
         // Update is called once per frame
@@ -28,6 +33,7 @@ namespace ProjectClicker
         public void TakeDamage(float damage)
         {
             health -= damage;
+            _healthBar.value = health;
             if (health < 0 && !isDead)
             {
                 isDead = true;
